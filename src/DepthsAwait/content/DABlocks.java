c66e4deb64b082ec0983577111f02a905e6207f3
Eredon302 <eredon2008@gmail.com>
2026-04-01 18:10:53 +0200

    :I

--- src/DepthsAwait/content/DABlocks.java
+++ src/DepthsAwait/content/DABlocks.java
@@ -1,5 +1,22 @@
 package DepthsAwait.content;
 
+import mindustry.type.*;
+import mindustry.world.Block;
+import mindustry.world.blocks.distribution.*;
+
+import static mindustry.type.ItemStack.with;
+
 public class DABlocks {
-    
+
+    //Item Transport
+    public static Block transferDuct;
+
+    public static void load(){
+        transferDuct = new Duct("transferDuct"){{
+            requirements(Category.distribution, with(DAItems.rhodite, 2));
+            health = 90;
+            speed = 4f;
+        }};
+    }
+
 }
